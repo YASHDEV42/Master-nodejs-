37ec97df912bc15a8cdb1cac7901dbe517f7d0dd
Yahya Shannat <yahyashannat@gmail.com>
2024-09-26 20:04:04 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
---
+++ routes/admin.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+const rootDir = require("../util/path");
+
+router.get("/add-product", (req, res, next) => {
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+});
+
+router.post("/add-product", (req, res, next) => {
+  console.log(req.body);
+
+  res.redirect("/");
+});
+
+module.exports = router;
