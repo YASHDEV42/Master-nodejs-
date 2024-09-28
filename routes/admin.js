9f4241a2d48aa82a3508b4ed9d58d8731646a496
Yahya Shannat <73648944+YASHDEV42@users.noreply.github.com>
2024-09-28 23:04:47 +0300

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,7 @@ router.get("/add-product", (req, res, next) => {
 
 router.post("/add-product", (req, res, next) => {
   console.log(req.body);
-
+  const data = req.body;
   res.redirect("/");
 });
 
